d866161683ed4b1328767fde62dba4aa286c1b0b
alireza-yn <alirezayousefnezhadian@gmail.com>
2025-03-27 20:03:59 +0330

    home added

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('',include('home.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/',include('contact.urls'))
 ]
