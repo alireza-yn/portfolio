0c9b1e6f0583b2f0976b3ea2f65cb007d8ab5dce
alireza-yn <alirezayousefnezhadian@gmail.com>
2025-03-27 20:27:13 +0330

    static fixed

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,8 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
+#develop mode
+# STATIC_URL = 'static/'
+# STATIC_ROOT = 'static'
+
+
+#production mode
+STATIC_ROOT = "/home/myusername/myproject/static"
+# or, eg,
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
